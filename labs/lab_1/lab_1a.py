6fb3f499a44b298e29b55c6910fee8380b3f3eb8
Srinikkesh S <srinikkeshsen@gmail.com>
2026-03-16 17:06:22 -0400

    A
    aa

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Srinikkesh Senthilkumar" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hi everyone! My name is Sri, and I am a sophomore from Pennsylvania. I have been interested in mechanical and aerospace engineering for as long as I can remember, and I find UAVs, morphing wings, and control theory to be particularly fascinating. I currently do VEX robotics and Science Olympiad for my school, and I used to do FTC as well. I also like to read, play chess, and get better at CAD, programming, and 3D printing through various projects. I look forward to what this course offers and collaborating with you all!")
 
 if __name__ == "__main__":
     main()
